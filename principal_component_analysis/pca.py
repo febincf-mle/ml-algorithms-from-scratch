f09d5f0219ad2e3b4c2365f9d1ac2a9acc20b3d1
febinnn <febin.web3dev@gmail.com>
2024-08-26 19:10:01 +0530

    Added alternate pca method in PCA class

--- principal_component_analysis/pca.py
+++ principal_component_analysis/pca.py
@@ -20,14 +20,18 @@ class PCA:
     def center_dataset(self, X):
         """ perform a centering of dataset by subtracting each data point from its mean value"""
         return X - X.mean(axis=0)
-
-
-    def compute_covariance_matrix(self, X):
-        """ Return the covariance matrix of the dataset """
-        return ( X.T @ X ) * (1 / X.shape[0]) 
+    
+    
+    def compute_covariance_matrix(self, X, alt=False):
+        """
+        Computes the matrix X @ X.T in case the d >> n\n
+        - Each of the eigenvalues of covariance will be scaled by n, because C = n * X @ X.T,
+        both XX.T and X.TX have the same eigenvalues"""
+        if alt: return np.dot(X, X.T)
+        return (1 / X.shape[0]) *  np.dot(X.T, X)
     
 
-    def eigen_decomposition(self, C):
+    def eigen_decomposition(self, C, alt=False):
         """Returns the eigenvalues and eigenvectors of the given Covariance-Matrix"""
         eigenvalues, eigenvectors = np.linalg.eigh(C)
 
@@ -35,18 +39,26 @@ class PCA:
         eigenvalues = eigenvalues[idx][: self.k_components]
         eigenvectors = eigenvectors[:, idx][:, :self.k_components]
 
+        if  alt:
+            normalized_eigenvectors = eigenvectors / np.sqrt(eigenvalues)
+            eigenvectors = np.dot(self._centered_dataset.T, normalized_eigenvectors)
+
         return eigenvalues, eigenvectors
 
 
     def fit(self, X):
         """Fits the dataset and compute the eigenvalues and eigenvectors"""
+
+        n, d = X.shape
+
+        alt = (d / n) > 100
+
         self._centered_dataset = self.center_dataset(X)
-        covariance_matrix = self.compute_covariance_matrix(self._centered_dataset)
+        
+        C = self.compute_covariance_matrix(self._centered_dataset, alt)
+        self._eigenvalues, self._eigenvectors = self.eigen_decomposition(C, alt)
 
-        self._eigenvalues, self._eigenvectors = self.eigen_decomposition(covariance_matrix)
-        return 0
 
-
-    def transform(self, centered_dataset, eigenvectors):
+    def transform(self):
         """Transforms the dataset by reducing the n dimenstion to specified k dimension"""
-        return centered_dataset @ eigenvectors
+        return self._centered_dataset @ self._eigenvectors
